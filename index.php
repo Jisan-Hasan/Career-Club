399ba3a6cd4eb8388207d6f7021d9fe0816fccf4
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-03 21:51:19 +0600

    downgrade bootstrap version

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ require 'database.php';
     <title>Home</title>
 
     <!-- Bootstrap css cdn -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/w3.css">
 
 
 </head>
@@ -23,7 +25,7 @@ require 'database.php';
     <h1 class="btn btn-primary">Hello</h1>
 
     <!-- Bootstrap Script CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 
 </body>
